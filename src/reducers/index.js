cd2bb4971e85e503b1545bf4762c3433c89d66b2
Antony Muriithi <antonymuriithi@Antonys-MacBook-Pro.local>
2019-05-23 18:07:56 +0300

    Adds tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from "redux";
 import user from "./userReducer";
-import office from "./officeReducer";
+import offices from "./officeReducer";
 
 export default combineReducers({
-  user: user,
-  office: office
+  user,
+  offices
 });
